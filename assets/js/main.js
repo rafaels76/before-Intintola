a80d9144cfb5c3e48f9eace980b8fb4513af0845
Rafael Sanchez <rafaels76@gmail.com>
2023-01-21 22:38:49 -0400

    script inicial

---
+++ assets/js/main.js
@@ -0,0 +1,69 @@
+var map = L.map("map").setView([0, 09], 2);
+var currentVisibility = '';
+const issIconDayLight = L.icon({iconUrl: "assets/img/iss.png", iconSize: [100, 100], iconAnchor: [50, 50]});
+const issIconEclipsed = L.icon({iconUrl: "assets/img/iss-e.png", iconSize: [100, 100], iconAnchor: [50, 50] });
+const issRadious = L.circle([0,0], 2200e3, {color: "#19630a", opacity: 0.3, weight:1, fillColor: "#19630a", fillOpacity: 0.1}).addTo(map); 
+const issMarker = L.marker([0, 0], { icon: issIconDayLight }).addTo(map);
+const urlISS = `https://api.wheretheiss.at/v1/satellites/25544`;
+const urlLeaflet = "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}";
+let firstTime = true;
+
+var t = L.terminator();
+t.addTo(map);
+
+L.tileLayer(
+    urlLeaflet,
+    {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: "mapbox/streets-v11",
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: config.API_TOKEN
+    }
+  ).addTo(map);
+
+
+
+async function getISS() {
+    const response = await fetch(urlISS);
+    const data = await response.json();
+    
+    const { altitude, latitude, longitude, velocity, visibility } = data;
+    issRadious.setLatLng([latitude, longitude]);
+    issMarker.setLatLng([latitude, longitude]);
+    // Establecer ubicacopn de la ISS en el mapa
+    map.setView([latitude, longitude]);
+    // Check if is the first load of the page
+    if (firstTime) {
+      map.setView([latitude, longitude], 3);
+      currentVisibility = visibility;
+      firstTime = false;
+      if(visibility == 'daylight'){
+        issMarker.setIcon(issIconDayLight);
+        document.getElementById('labelDayNight').innerText = visibility.charAt(0).toUpperCase() + visibility.slice(1);;
+      } else {
+        issMarker.setIcon(issIconEclipsed);
+        document.getElementById('labelDayNight').innerText = visibility.charAt(0).toUpperCase() + visibility.slice(1);;
+      }
+    }
+    //Establecer el icono de la estacion segun se dia o noche
+    if(currentVisibility != visibility){
+      currentVisibility = visibility;
+      if(visibility == 'daylight'){
+        issMarker.setIcon(issIconDayLight);
+        document.getElementById('labelDayNight').innerText = visibility.charAt(0).toUpperCase() + visibility.slice(1);;
+      } else {
+        issMarker.setIcon(issIconEclipsed);
+        document.getElementById('labelDayNight').innerText = visibility.charAt(0).toUpperCase() + visibility.slice(1);;
+      }
+    }
+    // Actualizar las labels con las coordenadas
+    document.getElementById('labelLatitude').innerText = latitude.toFixed(6);
+    document.getElementById('labelLongitude').innerText = longitude.toFixed(6);
+    document.getElementById('labelAltitude').innerText = altitude.toFixed(2);
+    document.getElementById('labelVelocity').innerText = velocity.toFixed(2);
+}
+
+getISS();
+setInterval(getISS, 1500);
---
+++ config.js
@@ -0,0 +1,3 @@
+var config = {
+    API_TOKEN:  'pk.eyJ1IjoicmFmYWVsczc2IiwiYSI6ImNrc3FyMzQ2ajBmb2Myb3FrMzV1YjhpdDEifQ.Iy0ikuINGfu5dStC-nwIow'
+}
